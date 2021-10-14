228abc96304d8f61d7612ca535949ad6b9dbe075
S <scottlydon18@gmail.com>
2021-10-13 22:43:50 -0700

    First commit

---
+++ BlaBlabla/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BlaBlabla
+//
+//  Created by Scott Lydon on 10/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("this is the first commit")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
